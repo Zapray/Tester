eb13d5a5683baecaa50a82e79d9e1b3e7ce4827b
Karin <karbro@student.chalmers.se>
2015-03-11 10:23:56 +0100

    mer skräp

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HellŒ world");
-		System.out.println("HŠŠŠj");
+		System.out.println("HŠŠŠj o svej");
 	}
 }
